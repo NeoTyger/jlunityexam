323eec184be19f51f5874cc7c60917b2b7377aba
JL Cardona <neojl333@gmail.com>
2022-11-30 11:36:43 +0100

    Implementado: E / En progreso: F

--- Assets/Challenge 1/Scripts/GameManager.cs
+++ Assets/Challenge 1/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour
     public GameObject player;
 
     private int playerLifes = 3;
+
+    private bool playerWins;
+
+    private Vector3 spawnOfTheLevel = Vector3.zero;
     
     // Start is called before the first frame update
     void Start()
@@ -26,11 +30,28 @@ public class GameManager : MonoBehaviour
     {
         playerLifes -= 1;
         Debug.Log("Actual lifes : " + playerLifes);
+        RestartLevel();
 
         if (playerLifes <= 0)
         {
             Destroy(player);
             Debug.Log("YOU DIE... GAME OVER");
+            
+            playerWins = false;
+            GameOver();
+        }
+    }
+
+    private void RestartLevel()
+    {
+        player.transform.position = spawnOfTheLevel;
+    }
+
+    private void GameOver()
+    {
+        if (playerWins == false)
+        {
+            Time.timeScale = 0;
         }
     }
 }
